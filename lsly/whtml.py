a36de51d6cd09eb895d4f03de0dd51bc2cd91717
neo <748909270@qq.com>
2015-04-09 12:17:54 +0800

    l1

--- lsly/whtml.py
+++ lsly/whtml.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-  
 from pyh import *
+page=PyH("目录")
+pyge.addMEAT()
+page.addJS("http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js")
+page.addJS("js/bootstrap.min.js")
+page<<h1('炼神领域',cl='center')
+page.printOut("test.html")
